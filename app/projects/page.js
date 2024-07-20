52bfb04c514ec90f2f9f73c5286598c4298ccfd3
mohamad.alturky <m799678@gmail.com>
2024-07-20 20:00:51 +0300

    adding design viewer route

---
+++ app/projects/page.js
@@ -0,0 +1,14 @@
+'use client'
+import '@xyflow/react/dist/style.css';
+import OverviewFlow from "../../design/OverviewFlow"
+
+export default function Projects() {
+  return (
+    <div style={{
+      // backgroundColor: "black",
+      height: "600px"
+    }}>
+      <OverviewFlow />
+    </div>
+  );
+}
