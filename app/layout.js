c26d55e3eb6093ac208a1be2c0647b8ea369d9ab
mohamad.alturky <m799678@gmail.com>
2024-07-20 09:20:44 +0300

    edit page layout

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import Header from "@/components/nav/Header";
+import Links from "@/components/nav/Links";
 
 export const metadata = {
   title: "Create Next App",
@@ -10,8 +9,18 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    // <html lang="en">
+    //   <body className={inter.className}>{children}</body>
+    // </html>
+    <html>
+      <body>
+        <Header></Header>
+        <Links></Links>
+
+
+        {children}
+
+      </body>
     </html>
   );
 }
